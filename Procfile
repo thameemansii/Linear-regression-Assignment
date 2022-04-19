0936a5fd9906fcd14430a3add368b2b42e0c0f29
thameemansii <43813506+thameemansii@users.noreply.github.com>
2022-04-19 15:22:35 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ginucorn app:app
---
+++ main.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import sklearn
+
+app = Flask(__name__)
+model = pickle.load(open('Boston_house_price.pickle','rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        feature = [float(i) for i in request.form.values()]
+        feature = [np.array(feature)]
+        y_pred = model.predict(feature)
+
+    return render_template('index.html', predict_price= "House price is {}".format(y_pred))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
